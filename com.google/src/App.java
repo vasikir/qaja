a7adb5d3a57c52aa044a454bb5bfd33d1d14d0e7
vp.kirnos <vp.kirnos@stalcom.com>
2021-09-03 21:58:01 +0300

    screenshoot creation

--- com.google/src/App.java
+++ com.google/src/App.java
@@ -1,5 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
   
 public class App {
       public static void main(String[] args) throws Exception {
@@ -7,6 +12,8 @@ public class App {
           System.setProperty("webdriver.chrome.driver", "C:\\Users\\vp.kirnos\\qaja\\github\\qaja\\com.google\\lib\\chromedriver.exe");    
           WebDriver driver = new ChromeDriver();
           driver.get("https://www.google.com");
+          File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+          FileUtils.copyFile(scrFile, new File("C:\\Users\\vp.kirnos\\qaja\\github\\qaja\\com.google\\lib\\screenshot.png"));
           Thread.sleep(5000);
           driver.quit();   
       }
